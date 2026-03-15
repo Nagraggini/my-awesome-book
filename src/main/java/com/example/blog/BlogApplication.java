5a19d6b4eca62b8d011eb0517f1aaa6e8c244e54
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-03-15 11:22:07 +0100

    Fixed some errors.

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -1,4 +1,4 @@
-package com.example.animal_shelter;
+package com.example.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
